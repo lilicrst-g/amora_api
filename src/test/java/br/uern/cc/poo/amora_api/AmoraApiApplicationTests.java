a58593c213e315fc4b1fce85995f403781b45cb7
Lisandra <lisa.crisilva@outlook.com>
2025-11-08 15:13:04 -0300

    first commit

---
+++ src/test/java/br/uern/cc/poo/amora_api/AmoraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uern.cc.poo.amora_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmoraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
